724073922829a819da0fe3642ef79208141845a8
nicrodriguezval <nicrodriguezval@gmail.com>
2023-10-27 14:08:58 -0500

    Code reduction using lombok

--- src/main/java/com/test/microservice/models/Bill.java
+++ src/main/java/com/test/microservice/models/Bill.java
@@ -1,11 +1,15 @@
 package com.test.microservice.models;
 
 import jakarta.persistence.*;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
+import lombok.*;
 
 @Entity
 @Table(name = "bills")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Bill {
     @Id
     @SequenceGenerator(
@@ -30,60 +34,4 @@ public class Bill {
             updatable = false
     )
     private User user;
-
-    public Bill() {
-    }
-
-    public Bill(Long id, Double totalAmount, String desc) {
-        this.id = id;
-        this.totalAmount = totalAmount;
-        this.desc = desc;
-    }
-
-    public Bill(Double totalAmount, String desc) {
-        this.totalAmount = totalAmount;
-        this.desc = desc;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Double getTotalAmount() {
-        return totalAmount;
-    }
-
-    public void setTotalAmount(Double totalAmount) {
-        this.totalAmount = totalAmount;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public String toString() {
-        return "Bill{" +
-                "id=" + id +
-                ", totalAmount=" + totalAmount +
-                ", desc='" + desc + '\'' +
-                ", user=" + user +
-                '}';
-    }
 }
--- src/main/java/com/test/microservice/models/User.java
+++ src/main/java/com/test/microservice/models/User.java
@@ -1,12 +1,18 @@
 package com.test.microservice.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 
 @Entity
 @Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class User {
     @Id
     @SequenceGenerator(
@@ -28,62 +34,4 @@ public class User {
             cascade = CascadeType.ALL
     )
     private List<Bill> bills;
-
-    public User() {
-    }
-
-    public User(Long id, String name, Integer age, String email) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.email = email;
-    }
-
-    public User(String name, Integer age, String email) {
-        this.name = name;
-        this.age = age;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
